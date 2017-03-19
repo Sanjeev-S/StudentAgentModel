1c9cf7536ce51e6df3808c1792cbb69acf4cf6ff
Sanjeev S <sanjeevsuresh1995@gmail.com>
2017-03-19 16:15:49 +0530

    Add a sample class

---
+++ src/test/java/in/ac/bitsgoa/agentmodel/Identity/StudentTest.java
@@ -0,0 +1,14 @@
+package in.ac.bitsgoa.agentmodel.Identity;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StudentTest {
+	String message = "Hello World";
+	Student student = new Student(message);
+
+	@Test
+	public void testPrintMessage() {
+		assertEquals(message, student.printMessage());
+	}
+}
