865ecec9f872c1f4f97d2ecefae29aea2f3787ce
Sanjeev S <sanjeevsuresh1995@gmail.com>
2017-03-21 11:17:40 +0530

    qulice added with license

--- src/test/java/in/ac/bitsgoa/agentmodel/identity/StudentTest.java
+++ src/test/java/in/ac/bitsgoa/agentmodel/identity/StudentTest.java
@@ -1,3 +1,9 @@
+/* 
+Copyright (c) 2017, Qulice.com
+Sanjeev, Prasad Talasila
+All rights reserved.
+This software is released under GPL 3.0
+*/
 package in.ac.bitsgoa.agentmodel.identity;
 
 import org.junit.Test;
